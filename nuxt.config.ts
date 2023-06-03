d6d9e52bec7a1bf8051ef60b7ae15fa86d952d7b
braden <imbradn@protonmail.com>
2023-06-03 12:21:23 -0700

    Setup Poppins font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['nuxt-font-loader'],
   css: ["@/assets/style/main.scss"],
   srcDir: "src/",
+  fontLoader: {
+    external: [
+      {
+        src: 'https://fonts.googleapis.com/css2?family=Poppins&display=swap',
+        family: "Poppins",
+      }
+    ]
+  }
 });
