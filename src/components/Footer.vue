cdc85c36b959c9db9689842c8acc34afdce747ba
braden <imbradn@protonmail.com>
2023-06-14 16:56:50 -0700

    Added footer and icons

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="site-footer">
+    <div class="row section">
+      <div class="footer-section">
+        <h1 class="footer-title">Links</h1>
+        <NuxtLink class="footer-item" to="/">Home Page</NuxtLink>
+        <a class="footer-item" href="https://www.bradn.dev/">bradn.dev</a>
+        <a class="footer-item" href="https://discord.gg/RcdcNxR4ez"
+          >Discord Server</a
+        >
+      </div>
+      <div class="footer-section">
+        <h1 class="footer-title">Other Products</h1>
+        <a class="footer-item" href="https://www.bradn.dev/">bradn.dev</a>
+        <a class="footer-item" href="https://docs.bradn.dev/">docs.bradn.dev</a>
+      </div>
+      <div class="footer-section">
+        <h1 class="footer-title">Credits</h1>
+        <a class="footer-item" href="https://github.com/bs-community/skinview3d"
+          >Skin Rendering</a
+        >
+        <a class="footer-item" href="https://mc-heads.net/">Face Rendering</a>
+        <a class="footer-item" href="https://api.hypixel.net/">Hypixel API</a>
+        <a class="footer-item" href="https://wiki.vg/Mojang_API">Mojang API</a>
+      </div>
+    </div>
+    <div class="row center padded">
+      <a class="boxed-icon" href="https://discord.gg/RcdcNxR4ez">
+        <Icon name="ic:baseline-discord"
+      /></a>
+      <a class="boxed-icon" href="https://www.bradn.dev/">
+        <Icon name="mdi:web"
+      /></a>
+      <a class="boxed-icon" href="https://docs.bradn.dev/">
+        <Icon name="material-symbols:unknown-document-outline"
+      /></a>
+    </div>
+    <div class="row cc center padded">
+      bradn © 2023 All Rights Reserved. | Not affiliated with Hypixel or Mojang
+    </div>
+  </footer>
+</template>
