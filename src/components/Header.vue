de0164a5dc4c1ddb8706440ac979458f11128cfc
braden <imbradn@protonmail.com>
2023-07-10 14:56:45 -0700

    Changed to mobile header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
         ><h1>bradn</h1></NuxtLink
       >
 
-      <div class="search mini">
+      <div class="search mini desktop-only">
         <form @submit.prevent="searchHeader" class="bar">
           <Icon @click="searchHeader" name="material-symbols:search-rounded" />
           <input
@@ -16,10 +16,28 @@
         </form>
       </div>
     </div>
+    <div class="mobile-drop-btn" @click="mobileDrop = !mobileDrop">
+      <Icon v-if="!mobileDrop" name="ci:hamburger-lg" />
+      <Icon v-else name="material-symbols:close-rounded" />
+    </div>
   </header>
+  <div :class="`mobile_header ${mobileDrop === true ? 'active' : ''}`">
+    <div class="mobile_header-body">
+      <div class="search mini">
+        <form @submit.prevent="search" class="bar">
+          <Icon name="material-symbols:search-rounded" />
+          <input type="text" placeholder="Find a user" v-model="searchName" />
+        </form>
+        <button @click="search">Search</button>
+      </div>
+      <div class="divider center" style="margin-top: 2rem !important"></div>
+      <NuxtLink to="/leaderboard">Leaderboards</NuxtLink>
+    </div>
+  </div>
 </template>
 
 <script setup>
+const mobileDrop = ref(false);
 const searchNameHeader = ref("");
 
 const searchHeader = () => {
